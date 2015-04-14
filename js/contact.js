400be4214bf032dea2623a9d4a862dd727ff571f
Ellen Dash <edash@cfl.rr.com>
2015-04-14 16:21:59 -0400

    initial pages for about and contact

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+// Contains the JavaScript functionality unique to the Menu page
+
+
+// This is called from main.js (document ready)
+// Should contain any page unique functionality 
+function pageDocReady () {
+
+}
+
