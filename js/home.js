b7d93b8913654674ac8c55ae98cce0e8761aac7a
Ellen Dash <edash@cfl.rr.com>
2015-04-14 12:09:25 -0400

    modified header and footer

--- js/home.js
+++ js/home.js
@@ -4,15 +4,6 @@
 // This is called from main.js (document ready)
 // Should contain any page unique functionality 
 function pageDocReady () {
-   $('.slider').fractionSlider({
-      'fullWidth':      true,
-      'controls':       false, 
-      'pager':        true,
-      'responsive':       true,
-      'dimensions':       "460,400",
-      'increase':       false,
-      'pauseOnHover':     false,
-      'slideEndAnimation':  true
-    });
+
 }
 
--- js/main.js
+++ js/main.js
@@ -3,4 +3,33 @@ $( document ).ready(function() {
 
     // run page specific functions
     pageDocReady();
+
+    // this applies to all pages
+    adjustNavStyles();
 });
+
+function adjustNavStyles() {
+    var width = $(window).width();
+    // thought the width should be the same as media query, but there is some kind of difference
+    // possibly due to width of scroll bar? so ... reducing the check by 19 pixels, assuming the 
+    // scroll is 20 px
+    // SO ... what would happen if there was no scroll present? this would be off by 19 px. 
+    // is there a way to check for scroll bar present?
+   if (width < 1081) {
+       $('.main-nav').addClass('btn');
+   }
+   else {
+       $('.main-nav').removeClass('btn');
+   }
+}
+
+/******************************
+*  Event Listeners
+******************************/
+
+// Item: Window
+// Trigger: resize
+// Action: Adjust navigation style classes
+
+window.addEventListener("resize", adjustNavStyles);
+
