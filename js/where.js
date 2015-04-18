22c6827a947c71451258ece67dfdaaae26f757a3
Ellen Dash <edash@cfl.rr.com>
2015-04-18 11:12:43 -0400

    cleanup for validation errors

--- js/where.js
+++ js/where.js
@@ -3,23 +3,6 @@
 
 // This is called from main.js (document ready)
 // Should contain any page unique functionality 
-function pageDocReadyALT () {
-  // google map container
-  //$('.gmap').mobileGmap();
-  $('.gmap').mobileGmap({
-    markers: [
-        {
-          position: 'center',
-          info: '<h2>Pizza Planet</h2>4575 Military Trail<br>Jupiter, FL 33458',
-          showInfo: true
-        }
-      ]
-  });
-
-  setupLocationClick();
-
-}
-
 function pageDocReady () {
   // google map container
   //$('.gmap').mobileGmap();
@@ -33,29 +16,28 @@ function pageDocReady () {
       ]
   });
 
-
   $('#gmap-mercury').mobileGmap({
     markers: [
         {
           position: 'center',
-          info: '<h2>Pizza Planet</h2>36 Shields Rd<br>Huntsville, AL 35811',
+          info: '<h2>Pizza Planet</h2>6070 Moores Mill Rd<br>Huntsville, AL 35811',
           showInfo: true
         }
       ]
   });
-  // need map to be showing to initialize. So create, then hide.
+  // need map to be visible to initialize. So create, then hide.
   $('#details-mercury').hide();
 
   $('#gmap-saturn').mobileGmap({
     markers: [
         {
           position: 'center',
-          info: '<h2>Pizza Planet</h2>726 E Sarah Dewitt Drl<br>Gonzales, TX 78629',
+          info: '<h2>Pizza Planet</h2>726 E Sarah Dewitt Dr<br>Gonzales, TX 78629',
           showInfo: true
         }
       ]
   });
-  // need map to be showing to initialize. So create, then hide.
+  // need map to be visible to initialize. So create, then hide.
   $('#details-saturn').hide();
 
   setupLocationClick();
@@ -75,9 +57,7 @@ function setupLocationClick () {
       $('#location-name').text(altText);
       
       // display the appropriate map section
-
       // hide the current map & show the selected map
-
       var hideThis = $('.current');
       var showThis = $('#details-' + altText.toLowerCase());
       hideThis.removeClass('current');
@@ -86,96 +66,30 @@ function setupLocationClick () {
       $(hideThis).hide();
       $(showThis).show();      // update the map
       if (altText == "Jupiter") {
-        // nothing here ... jupiter seems to always work
+        // nothing here ... jupiter seems to always work; plugin is a little quirky
       }
       else if (altText == "Mercury") {
         $('#gmap-mercury').mobileGmap({
-    markers: [
-        {
-          position: 'center',
-          info: '<h2>Pizza Planet</h2>36 Shields Rd<br>Huntsville, AL 35811',
-          showInfo: true
-        }
-      ]
-  });
-      }
+          markers: [
+              {
+                position: 'center',
+                info: '<h2>Pizza Planet</h2>6070 Moores Mill Rd<br>Huntsville, AL 35811',
+                showInfo: true
+              }
+            ]
+        });
+      } 
       else {  // default to Saturn
         $('#gmap-saturn').mobileGmap({
-    markers: [
-        {
-          position: 'center',
-          info: '<h2>Pizza Planet</h2>726 E Sarah Dewitt Drl<br>Gonzales, TX 78629',
-          showInfo: true
-        }
-      ]
-  });
-      } 
-
-      
-
-    });
-
-  }
-}
-
-function setupLocationClickALT () {
-  // position the figcaption out of view
-  var locationItems = $('.article-icon a');
-
-  for (i=0; i<locationItems.length; i++) {
-    $(locationItems[i]).click(function (item) {
-      var chosen = $(item.currentTarget).find('img');
-      var altText = $(chosen).attr('alt');
-      $('#location-name').text(altText);
-      
-      // display the appropriate map section
-
-      // hide the current map & show the selected map
-
-      var hideThis = $('.current');
-      var showThis = $('#details-' + altText.toLowerCase());
-      hideThis.removeClass('current');
-      showThis.addClass('current');
-
-      $(hideThis).hide(400);
-      $(showThis).show(400);
-
-      // update the map
-      if (altText == "Jupiter") {
-        $('.gmap').mobileGmap({
           markers: [
               {
                 position: 'center',
-                info: '<h2>Pizza Planet</h2>4575 Military Trail<br>Jupiter, FL 33458',
+                info: '<h2>Pizza Planet</h2>726 E Sarah Dewitt Dr<br>Gonzales, TX 78629',
                 showInfo: true
               }
             ]
         });
-      }
-      else if (altText == "Mercury") {
-        $('.gmap').mobileGmap({
-          markers: [
-              {
-                position: 'center',
-                info: '<h2>Pizza Planet</h2>36 Shields Rd<br>Huntsville, AL 35811',
-                showInfo: true
-              }
-            ]
-        });
-      }
-      else {  // default to Saturn
-        $('.gmap').mobileGmap({
-          markers: [
-              {
-                position: 'center',
-                info: '<h2>Pizza Planet</h2>726 E Sarah Dewitt Drl<br>Gonzales, TX 78629',
-                showInfo: true
-              }
-            ]
-        });
-      } 
-
-    });
-
-  }
+      } // end else
+    }); // end function definition
+  }  // end loop
 }
--- readme.txt
+++ readme.txt
@@ -3,4 +3,7 @@ URL:
 Resources:
   Home Page plugin - jQuery FractionSlider: http://jacksbox.de/stuff/
   Menu Page plugin - Magnific Popup: http://dimsemenov.com/plugins/magnific-popup/
+  Where Page plugin - jQuery Responsive Google Map: Mobile GMap: https://github.com/joggink/mobileGmap
+    NOTE: joggink is a little quirky at times; may need to slightly resize page to get map to redraw correctly
+          attempted debugging plugin, but unable to resolve issue
   Using <address> - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/address
